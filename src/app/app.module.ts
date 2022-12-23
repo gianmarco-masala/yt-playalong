0a3250a89572b74b7ab4c78efb37b262b6d2fc7f
gianmarco.masala <gianmarco.masala@yahoo.it>
2022-12-23 05:15:02 +0100

    removed home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,29 @@
 // Core
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Third-party
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AudioContextModule } from 'angular-audio-context';
 
 // Custom
 import { AudioSettingsComponent } from './audio-settings/audio-settings.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { HomeModule } from './home/home.module';
 import { ToastrModule } from 'ngx-toastr';
 import { NavbarModule } from './navbar/navbar.module';
+import { FxRackModule } from './fx-rack/fx-rack.module';
+import { MediaManagerModule } from './media-manager/media-manager.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AudioSettingsComponent,
-  ],
+  declarations: [AppComponent, AudioSettingsComponent],
   imports: [
     BrowserAnimationsModule,
-    AppRoutingModule,
-    // NgbModule,
     AudioContextModule.forRoot('playback'),
-    HomeModule,
     NavbarModule,
+    MediaManagerModule,
+    FxRackModule,
     ToastrModule.forRoot(),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
